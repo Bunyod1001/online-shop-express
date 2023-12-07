54ee03af9c6cad6ff077abf2891159e30a0cffc7
Bunyodbek <nematullayevbunyod1029@gamil.com>
2023-12-07 16:21:31 +0500

    Logout

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ import express from 'express';
 import { create, engine } from 'express-handlebars';
 import AuthRoutes from "./routes/auth.js"
 import ProductsRoutes from "./routes/products.js"
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv'
+import flash from "connect-flash"
+import session from "express-session"
+import varMiddleware from './middleware/var.middleware.js';
+import cookiParser from "cookie-parser"
+
+dotenv.config()
+
 
 const app = express();
 
@@ -14,11 +23,34 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', './views')
 
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
+app.use(express.json())
+app.use(cookiParser())
+app.use(session({secret:"Bunyod", resave:false ,saveUninitialized:false}))
+app.use(flash())
+app.use(varMiddleware)
+
+
 app.use(AuthRoutes )
 app.use(ProductsRoutes)
-
+const startApp = () => {
    
-
-const PORT = process.env.PORT || 4100
-app.listen(PORT, () => 
-console.log(`server is running on port : ${PORT}`))
+  
+  mongoose.set('strictQuery', false);
+  
+    mongoose
+      .connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => {
+        console.log("mongodb connection ");
+  
+        const PORT = process.env.PORT || 4100;
+        app.listen(PORT, () => console.log(`server is running on port : ${PORT}`));
+      })
+      .catch((error) => {
+        console.error("MongoDBga ulanishda xatolik:", error);
+      });
+  };
+  
+  startApp();
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,90 @@
 import {Router} from 'express'
+import User from '../models/User.js'
+import bcrypt from "bcrypt"
+import { generateJWTToken } from '../services/token.service.js'
 
 const router = Router()
 
 router.get ( '/login', (req, res) => {
-    res.render('login')
+    if(req.cookies.token){
+        res.redirect("/")
+        return
+    }
+    res.render('login',{
+        title:"Login | Bunyodbek",
+        isLogin: true,
+        loginError: req.flash("loginError"),
+       })
 })
 router.get ( '/register', (req, res) => {
-    res.render('register')
+    if(req.cookies.token){
+        res.redirect("/")
+        return
+    }
+    res.render('register',{
+        title:"Register | Bunyodbek",
+        isRegister:true,
+        registerError:req.flash("registerError"),
+       })
 })
 
-export default router
+router.get("/logout", ( req, res ) => {
+    res.clearCookie("token")
+    res.redirect("/")
+})
+
+router.post("/login", async(req, res) => {
+    const {email, password} = req.body
+    if(!email || !password) {
+        req.flash("loginError", "All fields is required")
+        res.redirect("/login")
+        return
+    } 
+
+    const exitUser = await User.findOne( {email})
+    if (!exitUser){
+        req.flash("loginError", "User not found")
+        res.redirect("/login")
+         return  
+    }
+    const isPassEqual = await bcrypt.compare(password, exitUser.password)
+    if (!isPassEqual) {
+        req.flash("loginError", "Password wrong")
+        res.redirect("/login")
+         return 
+    }
+
+    const token = generateJWTToken(exitUser._id);
+    res.cookie("token", token, {httpOnly:true, secure:true})
+    res.redirect("/")
+})
+
+router.post("/register", async (req, res) => {
+    const {firstname, lastname, email, password} = req.body
+    if(!firstname ||!lastname || !email || !password) {
+        req.flash("registerError", "All fields is required")
+        res.redirect("/register")
+        return
+    } 
+
+    const candidate = await User.findOne({email})
+    if(candidate) {
+        req.flash('registerError', 'User already exist')
+        res.redirect('/register')
+        return
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const userData = {
+        firstName: req.body.firstname,
+        lastName:req.body.lastname,
+        email:req.body.email,
+        password:hashedPassword,
+    }
+    const user = await User.create(userData);
+    const token = generateJWTToken(user._id);
+    res.cookie("token", token, {httpOnly:true, secure:true})
+    res.redirect('/')
+    })
+
+    export default router
